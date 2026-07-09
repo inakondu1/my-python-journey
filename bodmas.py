72394ee00d64d3ff35c1be33eaa63010362f679a
inakondu1 <inaisah66@gmail.com>
2026-07-09 21:06:28 +0100

    i used bodmas

---
+++ bodmas.py
@@ -0,0 +1,15 @@
+'''
+🏋️ Your ChallengeLet's write a formula to calculate the average score of a student.
+Imagine a student took 3 exams and got these scores: 80, 90, and 70. 
+To find the average, you must add all three scores together first, and then divide 
+the total by 3.Write a script that:Creates three variables with those hardcoded 
+exam scores: exam1 = 80, exam2 = 90, exam3 = 70.Calculates the average score using
+ a single line of math code. Hint: You will need to use parentheses () to force Python
+  to add before it divides.Prints the average.Reply with your code, and once you pass, 
+  we will unlock our final topic, Topic 5: The Math Module!
+  '''
+
+#   score = 80, 90, 70
+
+average = (80 + 90 + 70) / 3
+print(average)
