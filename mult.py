59914d79dcb1ab702ff5243d99713b4b358227f8
inakondu1 <inaisah66@gmail.com>
2026-07-09 09:34:25 +0100

    my fist * maths

---
+++ mult.py
@@ -0,0 +1,10 @@
+''' Your First ChallengeWrite a small Python script that calculates how many seconds 
+there are in 5 minutes.
+Use fixed numbers and the multiplication operator (*).
+'''
+minute = 5
+
+seconds = 60
+
+total =  minute * seconds
+print(total)
