ffaf422aa40493f28fbd210ce63ac57e12769f37
inakondu1 <inaisah66@gmail.com>
2026-07-10 02:05:51 +0100

    my square rootexe

---
+++ ex2.py
@@ -0,0 +1,11 @@
+'''
+
+Challenge 2: Hypotenuse CalculatorYou are calculating the side of a square structure. 
+The area of the square is 49 square meters. Write a script that finds the square root
+ of 49 to determine the length of one side.Expected output: 7.0
+ '''
+
+import math
+
+root_val = math.sqrt(49)
+print(root_val)
