e8dcb03e00e8b03ffb09f8477fe6d5aab7784917
inakondu1 <inaisah66@gmail.com>
2026-07-13 14:25:00 +0100

    adding {} to print

---
+++ q1.py
@@ -0,0 +1,13 @@
+'''
+Write a Python script that does the following:
+
+    Ask the user for their first name.
+
+    Ask the user for their favorite color.
+
+    Print a single, friendly sentence using both pieces of information. 
+    For example: "Hi Ina! It's awesome that your favorite color is blue."
+    '''
+name = (input('what is  your first name: '))
+color = (input('what is your favourite color: '))
+print('hi', name, 'it is awsome that your favorite color is', color)
